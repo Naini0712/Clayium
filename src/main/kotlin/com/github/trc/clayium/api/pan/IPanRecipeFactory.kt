b791f2bf82ad10bd5b0e5384ac7f728cca15510b
Tomotomo_ <55199184+Naini0712@users.noreply.github.com>
2025-01-13 19:13:55 +0900

    Update IPanRecipeFactory.kt

--- src/main/kotlin/com/github/trc/clayium/api/pan/IPanRecipeFactory.kt
+++ src/main/kotlin/com/github/trc/clayium/api/pan/IPanRecipeFactory.kt
@@ -16,5 +16,5 @@ interface IPanRecipeFactory {
      * @param stacks the inventory of the PAN Adapter.
      */
     fun getEntry(world: IBlockAccess, pos: BlockPos, stacks: List<ItemStack>,
-                 laserEnergy: Double, laserCostPerTick: ClayEnergy): IPanRecipe?
+                 calculateLaserEnergy: Pair<Double, ClayEnergy>): IPanRecipe?
 }
