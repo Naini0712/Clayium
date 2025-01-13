b80d4cde10fdd29cc6a527246ca55dfcc4919082
Tomotomo_ <55199184+Naini0712@users.noreply.github.com>
2025-01-13 16:12:52 +0900

    Update CPanRecipeFactory.kt

--- src/main/kotlin/com/github/trc/clayium/common/pan/factories/CPanRecipeFactory.kt
+++ src/main/kotlin/com/github/trc/clayium/common/pan/factories/CPanRecipeFactory.kt
@@ -15,7 +15,7 @@ object CPanRecipeFactory : IPanRecipeFactory {
     override fun getEntry(world: IBlockAccess, pos: BlockPos, stacks: List<ItemStack>, laserEnergy: Double, laserCostPerTick: ClayEnergy): IPanRecipe? {
         val metaTileEntity = world.getMetaTileEntity(pos)
         if (metaTileEntity is ClayReactorMetaTileEntity) {
-        return getEntryClayReactor(metaTileEntity, stacks, laserEnergy, laserCostPerTick)
+            return getEntryClayReactor(metaTileEntity, stacks, laserEnergy, laserCostPerTick)
         }
         val recipe = metaTileEntity
             ?.getCapability(ClayiumTileCapabilities.RECIPE_LOGIC, null)
