aaca5058d63c5343940f63ed03a7f96563a89c40
Tomotomo_ <55199184+Naini0712@users.noreply.github.com>
2025-01-13 20:36:24 +0900

    Update CPanRecipeFactory.kt

--- src/main/kotlin/com/github/trc/clayium/common/pan/factories/CPanRecipeFactory.kt
+++ src/main/kotlin/com/github/trc/clayium/common/pan/factories/CPanRecipeFactory.kt
@@ -29,8 +29,8 @@ object CPanRecipeFactory : IPanRecipeFactory {
     private fun getEntryClayReactor(clayReactor: ClayReactorMetaTileEntity, stacks: List<ItemStack>, laserEnergy: Double, laserCostPerTick: ClayEnergy): IPanRecipe? {
         val recipe = clayReactor.workable.recipeProvider.searchRecipe(Int.MAX_VALUE, stacks) ?: return null
 
-        val finalizedDuration = recipe.duration.toDouble() / (7864232445021.91 + 1.0)
-        val laserEnergyCost = 2841.6 * finalizedDuration
+        val finalizedDuration = recipe.duration.toDouble() / (laserEnergy + 7864232445021.91)
+        val laserEnergyCost = (laserCostPerTick + 2841.6) * finalizedDuration
         return PanRecipe(recipe.inputs, recipe.copyOutputs(), recipe.cePerTick * finalizedDuration + laserEnergyCost)
     }
 }
